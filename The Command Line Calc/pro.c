623acb03c639ee495dd78da7dd31a18325253dbd
ANSH-creator-ui <singhnegiansh@gmail.com>
2025-10-11 15:00:11 +0530

    initial commit

--- The Command Line Calc/pro.c
+++ The Command Line Calc/pro.c
@@ -1,20 +1,4 @@
-/*
- • ProblemDescription:
- The Command-Line Calculator Program is a C-based application
- designed to perform a wide range of mathematical operations. The
- system begins with a flowchart design to outline the program's
- workflow and logic. It includes basic arithmetic operations such as
- addition, subtraction, multiplication, and division, along with advanced
- mathematical functions like trigonometric calculations (sin, cos, tan)
- and logarithms (base 10). The program also supports matrix operations,
- enabling users to perform matrix addition and multiplication. A result
- management system is implemented to display outputs and store
- them in a file named calculator_results.txt for future reference. This
- project integrates key C programming concepts such as strings,
- pointers, functions and file handling to
- develop an efficient and user-friendly calculator*/
-
-
+#include <stdlib.h>
 #include <string.h>
  #include <stdio.h>
 #include <math.h>
---
+++ The Command Line Calc/readme.md
@@ -0,0 +1,14 @@
+## The Command Line Calculator
+- The Command-Line Calculator Program is a C-based application
+ designed to perform a wide range of mathematical operations. The
+ system begins with a flowchart design to outline the program's
+ workflow and logic. It includes basic arithmetic operations such as
+ addition, subtraction, multiplication, and division, along with advanced
+ mathematical functions like trigonometric calculations (sin, cos, tan)
+ and logarithms (base 10). The program also supports matrix operations,
+ enabling users to perform matrix addition and multiplication. A result
+ management system is implemented to display outputs and store
+ them in a file named calculator_results.txt for future reference. This
+ project integrates key C programming concepts such as strings,
+ pointers, functions and file handling to
+ develop an efficient and user-friendly calculator.
